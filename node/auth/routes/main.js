4d0915256b01f530fdcab24d3b09731e783ca668
raminicano <raminicayes@gmail.com>
2024-04-09 11:28:29 +0900

    change authentication

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -10,6 +10,41 @@ var connection = new mysql({
     database: process.env.database
 });
 
+
+function template_result(result, res) {
+    res.writeHead(200);
+    var template = `
+      <!DOCTYPE html>
+      <head>
+        <title>Error</title>
+        <meta charset="UTF-8">
+        <link type="text/css" rel="stylesheet" href="mystyle.css" />
+      </head>
+      <body>
+        <table border="1" style="margin:auto;">
+        <thead>
+          <tr><th>User ID</th><th>Password</th></tr>
+        </thead>
+        <tbody>
+        `;
+    for (var i = 0; i < result.length; i++) {
+        template += `
+            <tr>
+              <td>${result[i]['userid']}</td>
+              <td>${result[i]['passwd']}</td>
+            </tr>
+          `;
+    }
+    template += `
+        </tbody>
+        </table>
+      </body>
+      </html>
+    `;
+    res.end(template);
+}
+
+
 const app = express()
 
 app.use(bodyParser.json());
@@ -41,6 +76,34 @@ app.post('/login', (req, res) => {
 // register
 app.post('/register', (req, res) => {
     const { id, pw } = req.body;
+    if (id == "") {
+        res.redirect("register.html")
+    } else {
+        let result = connection.query('select * from user where userid=?', [id]);
+        if (result.length > 0) {
+            res.writeHead(200);
+            var templete = `
+                <!DOCTYPE html>                
+                <head>
+                    <meta charset="utf-8" />
+                    <title>Error</title>
+                </head>
+                <body>
+                    <div>
+                    <h3 style="margin-left: 30px">Register Failed</h3>
+                    <h4 style="margin-left: 30px">이미 존재하는 아이디입니다.</h4>
+                    <a href="register.html" style="margin-left: 30px">다시 시도하기</a>
+                    </div>
+                </body>
+                </html>
+            `;
+            res.end(templete);
+        } else {
+            result = connection.query('insert into user values (?, ?)', [id, pw]);
+            console.log(result);
+            res.redirect('/');
+        }
+    }
     const result = connection.query("insert into user values (?, ?)", [id, pw]);
     console.log(result);
     res.redirect('/');
@@ -50,23 +113,36 @@ app.post('/register', (req, res) => {
 app.get('/select', (req, res) => {
     const result = connection.query('select * from user');
     console.log(result);
-    res.send(result);
+    if (result.length == 0) {
+        template_nodata(res);
+    } else {
+        template_result(result, res);
+    }
 })
 
 // request O, query X
 app.post('/select', (req, res) => {
     const result = connection.query('select * from user');
     console.log(result);
-    res.send(result);
+    if (result.length == 0) {
+        template_nodata(res);
+    } else {
+        template_result(result, res);
+    }
 })
 
-// request O, query O
+
 app.get('/selectQuery', (req, res) => {
     const id = req.query.id;
     const result = connection.query("select * from user where userid=?", [id]);
     console.log(result);
-    res.send(result);
-})
+    // res.send(result);
+    if (result.length == 0) {
+        template_nodata(res);
+    } else {
+        template_result(result, res);
+    }
+});
 
 // request O, query O
 app.post('/selectQuery', (req, res) => {
@@ -74,7 +150,11 @@ app.post('/selectQuery', (req, res) => {
     // console.log(req.body);
     const result = connection.query("select * from user where userid=?", [id]);
     console.log(result);
-    res.send(result);
+    if (result.length == 0) {
+        template_nodata(res);
+    } else {
+        template_result(result, res);
+    }
 })
 
 // request O, query O
