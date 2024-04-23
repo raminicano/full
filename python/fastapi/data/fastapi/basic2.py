07b71aee584295767f8cba3559f8fc0051d158eb
raminicano <raminicayes@gmail.com>
2024-04-23 18:01:55 +0900

    python database test

---
+++ python/fastapi/data/fastapi/basic2.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic.main import BaseModel
+
+class HelloWorldRequest(BaseModel):
+    name : str
+    age : str
+
+app = FastAPI()
+
+@app.get('/')
+async def Hello():
+    return "Hello World~!!"
+
+@app.get(path='/hello/{name}')
+async def hellow_with_name(name:str):
+    return "Hello with name. your name is " + name
+
+@app.get(path='/hello')
+async def hellow_with_querystring(name:str):
+    return "Hello with name. your name is " + name
+
+@app.post(path='/hello/post')
+async def hellow_with_name(request: HelloWorldRequest):
+    return "Hello with post. your name is {}, your age is {}".format(request.name, request.age)
+
